2c895a80ec89d2ee4f68e5b2bc9ffd98319c2d35
HMikolaj <helinskimikolaj@gmail.com>
2022-03-09 11:55:53 +0100

    cv.commit

--- Watershed/Watershed.py
+++ Watershed/Watershed.py
@@ -1,3 +1,10 @@
 import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
+from matplotlib import image as mpimg
+img = cv.imread('Data/TapeA_registration.jpg')
+gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+ret, thresh = cv.threshold(gray,0,255,cv.THRESH_BINARY_INV+cv.THRESH_OTSU)
+cv.imshow('Data/TapeA_registration.jpg')
+
+#imgplot=plt.imshow(img)

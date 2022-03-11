566bbf8502e35c418d72fc5b3efce6075c50e761
TheoHuegens <huegens.theo@gmail.com>
2022-03-11 14:01:33 +0100

    cv.commit

--- Watershed/Watershed.py
+++ Watershed/Watershed.py
@@ -3,21 +3,25 @@ import cv2 as cv
 from matplotlib import pyplot as plt
 from matplotlib import image as mpimg
 
+#PARAMETERS:
+k_Kernel = 1
+k_OPN = 100
+k_DIL = k_OPN + 1
+K_dist = 0.5
+
 img = cv.imread(r'C:\Users\huege\Documents\GitHub\AE2223-I-DO8-Q3-4-\Data\TapeA_registration.jpg')
 
 gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
 ret, thresh = cv.threshold(gray,0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
 
-cv.imshow('Otsu',ret)
-
 # noise removal
-kernel = np.ones((1,1),np.uint8)
-opening = cv.morphologyEx(thresh,cv.MORPH_OPEN,kernel, iterations =2)
-    # sure background area
-sure_bg = cv.dilate(opening,kernel,iterations=3)
+kernel = np.ones((k_Kernel,k_Kernel),np.uint8)
+opening = cv.morphologyEx(thresh,cv.MORPH_OPEN,kernel, iterations =k_OPN)
+# sure background area
+sure_bg = cv.dilate(opening,kernel,iterations=k_DIL)
 # Finding sure foreground area
 dist_transform = cv.distanceTransform(opening,cv.DIST_L2,5)
-ret, sure_fg = cv.threshold(dist_transform,0.7*dist_transform.max(),255,0)
+ret, sure_fg = cv.threshold(dist_transform,K_dist*dist_transform.max(),255,0)
 # Finding unknown region
 sure_fg = np.uint8(sure_fg)
 unknown = cv.subtract(sure_bg,sure_fg)

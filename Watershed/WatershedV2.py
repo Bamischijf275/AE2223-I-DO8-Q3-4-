cb7804722494e55e38e38880a3c9f128fe3ef4b4
TheoHuegens <huegens.theo@gmail.com>
2022-03-16 10:42:27 +0100

    cv.commit

---
+++ Watershed/WatershedV2.py
@@ -0,0 +1,53 @@
+import numpy as np
+import cv2 as cv
+from matplotlib import pyplot as plt
+from matplotlib import image as mpimg
+import os
+
+#PARAMETERS:
+k_Kernel = 1
+k_OPN = 100
+k_DIL = k_OPN + 1
+K_dist = 0.5
+
+#open image
+path_script = os.path.dirname(__file__)
+path_relative = "..\Data\TapeA_registration.jpg"
+path = os.path.join(path_script, path_relative)
+img = cv.imread(path)
+#Cleanup
+imgPMSF = cv.pyrMeanShiftFiltering(img, 10, 2)
+cv.imshow('imagePMSF',imgPMSF)
+
+#Otsu binarization
+gray = cv.cvtColor(imgPMSF,cv.COLOR_BGR2GRAY)
+ret, thresh = cv.threshold(gray,0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
+
+#BG/FG/X separation
+kernel = np.ones((k_Kernel,k_Kernel),np.uint8)
+opening = cv.morphologyEx(thresh,cv.MORPH_OPEN,kernel, iterations =k_OPN)
+
+sure_bg = cv.dilate(opening,kernel,iterations=k_DIL)
+
+dist_transform = cv.distanceTransform(opening,cv.DIST_L2,5)
+ret, sure_fg = cv.threshold(dist_transform,K_dist*dist_transform.max(),255,0)
+
+sure_fg = np.uint8(sure_fg)
+unknown = cv.subtract(sure_bg,sure_fg)
+
+#cv.imshow('sureBG',sure_bg)
+#cv.imshow('sureFG',sure_fg)
+#cv.imshow('Unknown',unknown)
+
+# Marker labelling
+ret, markers = cv.connectedComponents(sure_fg)
+markers = markers+1
+markers[unknown==255] = 0
+
+#Watershed and output
+markers = cv.watershed(img,markers)
+img[markers == -1] = [255,0,0]
+cv.imshow('image',img)
+
+#end
+cv.waitKey(0)

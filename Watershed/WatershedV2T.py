34ce517210875546e5ca0ae652def8a6ccc2d351
TheoHuegens <huegens.theo@gmail.com>
2022-03-18 09:15:30 +0100

    cv.commit

--- Watershed/WatershedV2T.py
+++ Watershed/WatershedV2T.py
@@ -1,37 +1,37 @@
-import numpy as np
 import cv2 as cv
+import imutils
+import numpy as np
 # from matplotlib import pyplot as plt
 # from matplotlib import image as mpimg
 import os
-import imutils
+import statistics as stat
+from scipy import ndimage
 from skimage.feature import peak_local_max
 from skimage.morphology import watershed
-from scipy import ndimage
-import statistics as stat
 
-# PARAMETERS:
-# assumption:
+# CONSTANTS:
+# parameters:
 F_mean = 5.4
-F_delta = 0.3 #% deviation
+F_RE = 0.3  # % deviation
+F_perPixel = 0.7256  # 1)<- 2) 0.7232
 
-R_min = F_mean * (1 - F_delta)
-R_max = F_mean * (1 + F_delta)
-MinDist = int(R_min-1)
+R_min = F_mean * (1 - F_RE)
+R_max = F_mean * (1 + F_RE)
+MinDist = int(R_min - 1)
 
-PyrFilt1 = 10
-PyrFilt2 = 10
+PyrFilt1 = 10  # iterations
+PyrFilt2 = 10  # strength
 
 # Display
-Show_In =           True
-Show_Otsu =         True
-Show_PyrFilt =      True
-Show_Boundary =     True
-Show_ShapeCenter =  True
-Show_FiberCircle =  True
+Show_In = True
+Show_Otsu = True
+Show_PyrFilt = True
+Show_Boundary = True
+Show_ShapeCenter = True
+Show_FiberCircle = True
 Col_ShapeCenter = (255, 0, 0)
 Col_Boundary = (0, 255, 0)
 Col_FiberCircle = (0, 0, 255)
-
 
 # IMAGE PROCESSING
 # Open image
@@ -41,14 +41,13 @@ path = os.path.join(path_script, path_relative)
 img = cv.imread(path)
 if Show_In: cv.imshow('INPUT', img)
 
-# Cleanup
+# Smoothing / de-noising
 imgPMSF = cv.pyrMeanShiftFiltering(img, PyrFilt1, PyrFilt2)
 if Show_PyrFilt: cv.imshow('imagePMSF', imgPMSF)
 
 # Otsu binarization
 gray = cv.cvtColor(imgPMSF, cv.COLOR_BGR2GRAY)
-imgTSH = cv.threshold(gray, 0, 255,
-                      cv.THRESH_BINARY | cv.THRESH_OTSU)[1]
+imgTSH = cv.threshold(gray, 0, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)[1]
 if Show_Otsu:
     cv.imshow("Thresh", imgTSH)
 
@@ -78,32 +77,32 @@ for label in np.unique(labels):
     # eliminate background
     if label == 0:
         continue
-    
-    #mask and label region
+
+    # mask and label region
     mask = np.zeros(gray.shape, dtype="uint8")
     mask[labels == label] = 255
-    
-    #find contours
+
+    # find contours
     cnts = cv.findContours(mask.copy(), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
     c = max(cnts, key=cv.contourArea)
-    
-    #fit enclosing circle
+
+    # fit enclosing circle
     ((x, y), r) = cv.minEnclosingCircle(c)
     if Show_ShapeCenter:
         cv.circle(img, (int(x), int(y)), 1, Col_ShapeCenter, 0)
-    
+
     # fiber ID and appending
     S.add(r)
     if R_min < r < R_max:
         if Show_FiberCircle:
             cv.circle(img, (int(x), int(y)), int(r), Col_FiberCircle, 1)
             # cv.putText(img, "#{}".format(label), (int(x) - 10, int(y)),
-                # cv.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
+            # cv.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
         F.add(r)
         Fibers.append([x, y, round(r, 1)])
 # STATISCTICS:
-        
+
 S_med = stat.median(S)
 S_sigma = stat.stdev(S)
 S_avg = stat.mean(S)

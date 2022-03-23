2d431655ae37b1984c6943db9f0e2c7566e7997d
TheoHuegens <huegens.theo@gmail.com>
2022-03-23 11:32:37 +0100

    cv.commit

--- Watershed/WatershedV2TF.py
+++ Watershed/WatershedV2TF.py
@@ -10,7 +10,7 @@ from skimage.feature import peak_local_max
 from skimage.segmentation import watershed
 #import sys
 
-def WATERSHED (Name, Filetype, PathIN, pathOUT):
+def WATERSHED (Name, Filetype, PathIN, pathOUT, PRINT):
 
     # PARAMETER DEFINITION
     F_mean = 5.4

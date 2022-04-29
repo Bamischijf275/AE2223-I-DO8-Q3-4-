596feb38d2507a9243d271d07781e0cefa03d387
Jurgen de Groot <Degrootjurgen39@gmail.com>
2022-04-29 13:24:43 +0200

    Created CSV_merger.py, which can combine CSV patches into the original picture size. Still needs some fixing.

---
+++ Data Processed/CSV_merger.py
@@ -0,0 +1,37 @@
+import numpy as np
+from PIL import Image
+import csv
+
+directory = "GroundTruth"
+
+def csv_to_numpy(filename):
+#----------------------------------------------------
+# Convert a csv file to a numpy array
+#----------------------------------------------------
+    file = open(filename)
+    file_npar = np.loadtxt(file,delimiter=",")
+    return file_npar
+
+
+def combine_csv(j):
+# ----------------------------------------------------
+#Grab 10 patches of a picture, convert them to ndarray format
+#Combine top and bottom with np.hstack, then add two horizntal patches wiht np.vstack
+#Remove .jpg.tif from the filename as they are redundant
+# ----------------------------------------------------
+    new_ar_top = csv_to_numpy(f"GroundTruth/Tape_B_{j}_{1}.jpg.tif.csv")
+    new_ar_bot = csv_to_numpy(f"GroundTruth/Tape_B_{j}_{6}.jpg.tif.csv")
+    for i in range(2,6):
+         print(np.shape(csv_to_numpy(f"GroundTruth/Tape_B_{j}_{i}.jpg.tif.csv")))
+         new_ar_top = np.hstack((new_ar_top,csv_to_numpy(f"GroundTruth/Tape_B_{j}_{i}.jpg.tif.csv")))
+    for h in range(7,11):
+        new_ar_bot = np.hstack((new_ar_bot,csv_to_numpy(f"GroundTruth/Tape_B_{j}_{h}.jpg.tif.csv")))
+    return_ar = np.vstack((new_ar_top,new_ar_bot))
+    print(np.shape(return_ar))
+    return return_ar
+
+#Execute
+#Comment = File has been executed, but several numbers are still missing here.
+for o in range(1,10):
+    a = combine_csv(o)
+    np.savetxt(f'GroundTruth/Merged CSV/Tape_B_{o}.csv',a,delimiter=",")

81f8927c40a52d01b221d990497c5a876b404f3f
ryan-bergmans <46223973+ryanfire2123@users.noreply.github.com>
2022-05-06 15:17:53 +0200

    Plotting class, again

--- Plotting.py
+++ Plotting.py
@@ -1,8 +1,25 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+SD1 = [0.4, 0.5, 0.45, 0.55]
+SD2 = [0.42, 0.52, 0.47, 0.57]
+SD3 = [0.38, 0.48, 0.43, 0.53]
+WS = [0.3, 0.4, 0.4, 0.4]
+
 
 def generateResultsChart(SD1, SD2, SD3, WS):
+
+    SD1min = [0.1, 0.1, 0.1, 0.1]
+    SD1max = [0.2, 0.2, 0.2, 0.2]
+
+    SD2min = [0.1, 0.1, 0.1, 0.1]
+    SD2max = [0.1, 0.1, 0.1, 0.1]
+
+    SD3min = [0.1, 0.1, 0.1, 0.1]
+    SD3max = [0.1, 0.1, 0.1, 0.1]
+
+    WSmin = [0.1, 0.1, 0.1, 0.1]
+    WSmax = [0.1, 0.1, 0.1, 0.1]
 
     labels = [r'$\alpha$', r'$\beta$', r'$\gamma$', r'$\delta$']
 
@@ -10,11 +27,18 @@ def generateResultsChart(SD1, SD2, SD3, WS):
     width = 0.15  # the width of the bars
 
     fig, ax = plt.subplots()
-    SD1bar = ax.bar(x - (3*width/2), SD1, width, label='StarDist1')
-    SD2bar = ax.bar(x - width/2, SD2, width, label='StarDist2')
-    SD3bar = ax.bar(x + width/2, SD3, width, label='StarDist3')
-    WSbar = ax.bar(x + (3*width/2), WS, width, label='Watershed')
 
+    SD1bar = ax.bar(x - (3*width/2), SD1, width, label='StarDist1')
+    ax.errorbar(x - (3*width/2), SD1, yerr=[SD1min, SD1max], fmt='ko', capsize=5)
+
+    SD2bar = ax.bar(x - width/2, SD2, width, label='StarDist2')
+    ax.errorbar(x - width/2, SD2, yerr=[SD2min, SD2max], fmt='ko', capsize=5)
+
+    SD3bar = ax.bar(x + width/2, SD3, width, label='StarDist3')
+    ax.errorbar(x + width/2, SD3, yerr=[SD3min, SD3max], fmt='ko', capsize=5)
+
+    WSbar = ax.bar(x + (3*width/2), WS, width, label='Watershed')
+    ax.errorbar(x + (3*width/2), WS, yerr=[WSmin, WSmax], fmt='ko', capsize=5)
 
     # Add some text for labels, title and custom x-axis tick labels, etc.
     ax.set_ylabel('Value [-]')
@@ -22,13 +46,10 @@ def generateResultsChart(SD1, SD2, SD3, WS):
     ax.set_xticklabels(labels)
     ax.legend()
 
-    ax.bar_label(SD1bar, padding=3)
-    ax.bar_label(SD2bar, padding=3)
-    ax.bar_label(SD3bar, padding=3)
-    ax.bar_label(WSbar, padding=3)
-
-
     fig.tight_layout()
 
-    plt.ylim(0.4, 1)
+    plt.ylim(0.3, 0.8)
     plt.savefig("EffectivenessOfDifferentMethods")
+
+
+generateResultsChart(SD1, SD2, SD3, WS)
